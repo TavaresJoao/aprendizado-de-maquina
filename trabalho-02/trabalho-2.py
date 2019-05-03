d7fc2bd669283d1391a5397f6c06e4b6ca9d327e
Joao Tavares <joao_stavares@outlook.com>
2019-05-03 20:43:31 -0300

    Trabalho 2 update

--- trabalho-02/trabalho-2.py
+++ trabalho-02/trabalho-2.py
@@ -15,7 +15,6 @@ fruits = pd.read_csv('database.csv');
 # Alguns atributos de database são alfa numéricos, portanto devemos deixá-los
 # de forma muerica e para isso usa-se. O método *factorize* divide dois termos
 # o dumbs separa para mais termos.
-
 fruits['class'] = pd.factorize(fruits['class'])[0]
 fruits['taste'] = pd.factorize(fruits['taste'])[0]
 fruits['Color-F'] = pd.factorize(fruits['Color-F'])[0]
@@ -23,10 +22,26 @@ fruits = pd.get_dummies(fruits, columns=['Color-D'])
 fruits['texture'] = pd.factorize(fruits['texture'])[0]
 
 # Agora deve-se separar o vetor de rótulos
-
-y = fruits['class'].values
+y = fruits['class']
 
 fruits = fruits.drop(['class'],axis=1)
-X = fruits.iloc[:,:].values
+X = fruits.iloc[:,:]
 
-#%%
+
+# Salva-se os dados para nao ser necessário fazer o pre-processamento 
+# futuramente
+X.to_csv('dados.csv')
+y.to_csv('rotulos.csv')
+
+#%% Carregando os dados salvos
+y = pd.read_csv('rotulos.csv')
+y = y['0.1'].values
+
+X = pd.read_csv('dados.csv')
+X = X.drop(['Unnamed: 0'],axis=1)
+X = X.iloc[:,:].values
+
+#%% Fazer a base de traino e testes
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test =
+        train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
