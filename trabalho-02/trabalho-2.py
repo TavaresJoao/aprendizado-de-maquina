f67233bf10ee621c3fefb6b419d1ec1d8d636d0a
TavaresJoao <joao_stvares@outlook.com>
2019-05-13 17:03:27 -0300

    Update trabalho-2.py

--- trabalho-02/trabalho-2.py
+++ trabalho-02/trabalho-2.py
@@ -14,7 +14,7 @@ fruits = pd.read_csv('database.csv');
 
 # Alguns atributos de database são alfa numéricos, portanto devemos deixá-los
 # de forma muerica e para isso usa-se. O método *factorize* divide dois termos
-# o dumbs separa para mais termos.
+# o dummiess separa para mais termos.
 fruits['class'] = pd.factorize(fruits['class'])[0]
 fruits['taste'] = pd.factorize(fruits['taste'])[0]
 fruits['Color-F'] = pd.factorize(fruits['Color-F'])[0]
@@ -34,14 +34,67 @@ X.to_csv('dados.csv')
 y.to_csv('rotulos.csv')
 
 #%% Carregando os dados salvos
-y = pd.read_csv('rotulos.csv')
-y = y['0.1'].values
+# isso não está funcionando, pois o y está sendo salvo com uma linha a menos
+#y = pd.read_csv('rotulos.csv')
+#y = y['0.1'].values
 
-X = pd.read_csv('dados.csv')
-X = X.drop(['Unnamed: 0'],axis=1)
-X = X.iloc[:,:].values
+#X = pd.read_csv('dados.csv')
+#X = X.drop(['Unnamed: 0'],axis=1)
+#X = X.iloc[:,:].values
 
 #%% Fazer a base de traino e testes
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test =
-        train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+#%%
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+#%% Treinar o clássificador pleo métdo Stochastic Gradient Descent (SGD)
+from sklearn.linear_model import Perceptron
+
+perceptron = Perceptron(tol=1e-3, random_state=0, max_iter=10)
+perceptron.fit(X_train_std, y_train)
+
+#%% Predict
+y_train_pred = perceptron.predict(X_train_std)
+y_test_pred = perceptron.predict(X_test_std)
+
+#%% Mostrar o resultado
+from sklearn.metrics import accuracy_score
+
+print(accuracy_score(y_train_pred, y_train))
+print(accuracy_score(y_test_pred, y_test))
+
+#%% Fazer um gráfico
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.model_selection import train_test_split
+
+heldout = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+
+for i in heldout:
+    clf = Perceptron(tol=1e-3, max_iter=i)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+    
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+    
+    clf.fit(X_train_std, y_train)
+    
+    print(i)
+    print(clf.score(X_train_std, y_train))
+    print(clf.score(X_test_std, y_test))
+
+    
