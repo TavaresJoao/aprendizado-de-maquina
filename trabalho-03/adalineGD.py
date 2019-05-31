a26e40656dab6887616b5a5bd96ab60ac8eb151c
TavaresJoao <joao_stvares@outlook.com>
2019-05-31 16:43:05 -0300

    perceptron and adaline update

--- trabalho-03/adalineGD.py
+++ trabalho-03/adalineGD.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Fri May 31 11:42:06 2019
 
-Este é um arquivo de script temporário.
+@author: joao_
 """
 
-#%% Includes
+#%%
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-#%% Perceptron Class
-class Percecptron(object):
-    
+#%% Adaline class
+class AdalineGD(object):
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
@@ -23,32 +22,33 @@ class Percecptron(object):
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01,
                               size=1+X.shape[1])
-        self.errors_ = []
+        self.cost_ = []
         
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+        for i in range(self.n_iter):
+            net_input   = self.net_input(X)
+            output      = self.activation(net_input)
+            errors      = (y-output)
+            self.w_[1:] += self.eta * X.T.dot(net_input)
+            self.w_[0]  += self.eta * errors.sum()
+            cost = (errors**2).sum() /2.0
+            self.cost_.append(cost)
+            
         return self
     
     def fit_plot(self, X, y):
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01,
                               size=1+X.shape[1])
-        self.errors_ = []
+        self.cost_ = []
         
         for i in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+            net_input   = self.net_input(X)
+            output      = self.activation(net_input)
+            errors      = (y-output)
+            self.w_[1:] += self.eta * X.T.dot(net_input)
+            self.w_[0]  += self.eta * errors.sum()
+            cost = (errors**2).sum() /2.0
+            self.cost_.append(cost)
             
             self.plot_decision_regions(X, y)
             
@@ -57,6 +57,7 @@ class Percecptron(object):
             plt.legend(loc='upper left')
             plt.title('n_iter='+str(i+1))
             plt.show()
+        
         return self
     
     def plot_decision_regions(self, X, y, resolution=0.02):
@@ -90,12 +91,15 @@ class Percecptron(object):
         return
     
     def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+        return np.dot(X, self.w_[1:])+self.w_[0]
+    
+    def activation(self, X):
+        return X
     
     def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
     
-#%% Data Set
+#%% Data set
 df = pd.read_csv('https://archive.ics.uci.edu/ml/'
                  'machine-learning-databases/iris/iris.data', header=None)
 df.tail()
@@ -119,65 +123,8 @@ plt.xlabel('sepal length [cm]')
 
 plt.show()
 
-#%% Trainning the Perceptron Model
-ppn = Percecptron(eta=0.1, n_iter=10)
+#%% Trainning the AdalineGD Model
+ada = AdalineGD(eta=0.001, n_iter=10)
 
-ppn.fit(X, y)
+ada.fit_plot(X, y)
 
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of updates')
-
-plt.show()
-
-#%% Function for plotting decision regions
-def plot_decision_regions(X, y, classifier, resolution=0.02):
-
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot class samples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], 
-                    y=X[y == cl, 1],
-                    alpha=0.8, 
-                    c=colors[idx],
-                    marker=markers[idx], 
-                    label=cl, 
-                    edgecolor='black')
-
-
-
-
-plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc='upper left')
-
-
-# plt.savefig('images/02_08.png', dpi=300)
-plt.show()
-
-#%% Trabalho-2
-ppn = Percecptron(eta=0.1, n_iter=10)
-
-ppn.fit_plot(X, y)
-
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of updates')
-
-plt.show()
