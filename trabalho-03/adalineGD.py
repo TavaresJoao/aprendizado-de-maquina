1954c2695d5bda21c32faacc68929a5d7c736033
TavaresJoao <joao_stvares@outlook.com>
2019-05-31 18:28:06 -0300

    Update adalineGD.py

--- trabalho-03/adalineGD.py
+++ trabalho-03/adalineGD.py
@@ -17,37 +17,34 @@ class AdalineGD(object):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
-        
+
     def fit(self, X, y):
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01,
-                              size=1+X.shape[1])
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.cost_ = []
-        
+
         for i in range(self.n_iter):
-            net_input   = self.net_input(X)
-            output      = self.activation(net_input)
-            errors      = (y-output)
-            self.w_[1:] += self.eta * X.T.dot(net_input)
-            self.w_[0]  += self.eta * errors.sum()
-            cost = (errors**2).sum() /2.0
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
             self.cost_.append(cost)
-            
         return self
     
     def fit_plot(self, X, y):
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01,
-                              size=1+X.shape[1])
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.cost_ = []
-        
+
         for i in range(self.n_iter):
-            net_input   = self.net_input(X)
-            output      = self.activation(net_input)
-            errors      = (y-output)
-            self.w_[1:] += self.eta * X.T.dot(net_input)
-            self.w_[0]  += self.eta * errors.sum()
-            cost = (errors**2).sum() /2.0
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
             self.cost_.append(cost)
             
             self.plot_decision_regions(X, y)
@@ -57,11 +54,9 @@ class AdalineGD(object):
             plt.legend(loc='upper left')
             plt.title('n_iter='+str(i+1))
             plt.show()
-        
         return self
     
     def plot_decision_regions(self, X, y, resolution=0.02):
-        
         # setup marker generator and color map
         markers = ('s', 'x', 'o', '^', 'v')
         colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -89,16 +84,17 @@ class AdalineGD(object):
                         label=cl, 
                         edgecolor='black')
         return
-    
+
     def net_input(self, X):
-        return np.dot(X, self.w_[1:])+self.w_[0]
-    
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
     def activation(self, X):
         return X
-    
+
     def predict(self, X):
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
-    
+
+
 #%% Data set
 df = pd.read_csv('https://archive.ics.uci.edu/ml/'
                  'machine-learning-databases/iris/iris.data', header=None)
@@ -124,7 +120,80 @@ plt.xlabel('sepal length [cm]')
 plt.show()
 
 #%% Trainning the AdalineGD Model
-ada = AdalineGD(eta=0.001, n_iter=10)
+# standardize features
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
-ada.fit_plot(X, y)
+ada = AdalineGD(eta=0.01, n_iter=20)
 
+ada.fit(X_std, y)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+# plt.savefig('images/02_11.png', dpi=300)
+plt.show()
+
+#%% Function for plotting decision regions
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+
+
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+
+# plt.savefig('images/02_08.png', dpi=300)
+plt.show()
+
+#%% TRabalho-3
+#%% Trainning the AdalineGD Model
+# standardize features
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+
+ada = AdalineGD(eta=0.01, n_iter=20)
+
+ada.fit_plot(X_std, y)
